91a2dc18a676a3c0ef92ed166d5a0905c947f179
Alex <admin@Alexs-MacBook.local>
2023-03-27 17:40:52 +0200

    first modal done

--- script.js
+++ script.js
@@ -1,76 +1,35 @@
-$(function () {
-  // // Vars
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
 
-  // let header = $("#header");
-  // let introH = $("#intro").innerHeight();
-  // let scrollOffset = $(window).scrollTop();
-
-  // // Fixed Header
-
-  // $(window).on("scroll load resize", function () {
-  //   scrollOffset = $(this).scrollTop();
-
-  //   checkScroll(scrollOffset);
-  // });
-
-  // function checkScroll(scrollOffset) {
-  //   if (scrollOffset >= introH) {
-  //     header.addClass("fixed");
-  //   } else {
-  //     header.removeClass("fixed");
-  //   }
-  // }
-
-  // checkScroll(scrollOffset);
-
-  // // Smooth scroll
-  // // $("[data-scroll]").on("click", function (event) {
-  // //   event.preventDefault();
-  // //   let $this = $(this);
-  // //   let blockId = $this.data("scroll");
-  // //   let blockOffset = $(blockId).offset().top;
-
-  // //   $("#nav a").removeClass("active");
-  // //   $this.addClass("active");
-
-  // //   $("html,body").animate(
-  // //     {
-  // //       scrollTop: blockOffset - 30,
-  // //     },
-  // //     600
-  // //   );
-  // // });
-
-  // // Burger toggle
-  // $("#nav__toggle").on("click", function (event) {
-  //   event.preventDefault();
-
-  //   $(this).toggleClass("active");
-  //   $("#nav").toggleClass("active");
-  // });
-  // // Burger collapse
-
-  // $(".nav__link,  #header a").on("click", function (event) {
-  //   event.preventDefault();
-  //   $("#nav__toggle, #nav").removeClass("active");
-  // });
-
-  // // Accordion collapse
-  // $("[data-collapse]").on("click", function (event) {
-  //   event.preventDefault();
-
-  //   let $this = $(this);
-  //   let blockId = $this.data("collapse");
-  //   $this.toggleClass("active");
-  // });
-
-  // Slider
-  $("[data-slider]").slick({
-    infinite: true,
-    fade: false,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 300,
-    dots: true,
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
   });
 });
+
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal-contacts.active ");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal-contacts");
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
